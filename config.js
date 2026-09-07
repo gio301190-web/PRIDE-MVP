ed40f155f051ff673ed440c0662f942fd33fe820
gio301190-web <gio301190@gmail.com>
2026-09-08 02:13:55 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PRIDE_CONFIG = {
-  SUPABASE_URL: "https://gkokppsjgsnedkodvve.supabase.co",
+  SUPABASE_URL: "https://gkokppsjngsnedkodvve.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_0JcPtXEWa1_4mDiy7zO4fQ_ccFOZoIG"
 };
